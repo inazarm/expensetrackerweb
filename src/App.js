5e075413f0c4b66dab0a61e906a2d9c19bf5c879
Nazar.M <inazar.m@gmail.com>
2020-09-02 16:30:16 +0500

    Header Added and css file added for complete project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+//Import Components
+import {Header} from './components/Header';
 
 function App() {
   return (
     <div>
-
+      <Header></Header>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <h1>
+            Expense Tracker By Nazar
+        </h1>
+    )
+}
+export default 'Header';
